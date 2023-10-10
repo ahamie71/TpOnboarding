ac1904e613480ac53057559114ea485bc8c4ac99
ahamie71 <ahmad.hamieh2002@hotmail.com>
2023-10-10 16:44:32 +0200

    link css

--- pages/listProducts.php
+++ pages/listProducts.php
@@ -2,16 +2,15 @@
 include '../include/navbar.php';
 ?>
 
-    
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="../public/css/listproducts.css">
     <title>Document</title>
+
+    <!-- Add Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     
